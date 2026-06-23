a5ffb1bb885cabf1a004bc719ca9f196fdb86e32
QL1900 <ql1900@users.noreply.github.com>
2026-06-23 09:12:59 +0800

    Add Aliyun Maven mirrors for faster dependency resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,16 @@
-pluginManagement {
+﻿pluginManagement {
     repositories {
-        google()
-        mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
         google()
         mavenCentral()
     }
